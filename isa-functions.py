f8d5c92f8e98fb80585347369c5c2a64afb2b2d8
Isabella <98288767+ilu24@users.noreply.github.com>
2025-06-16 13:18:31 -0400

    Create isa-functions.py

---
+++ isa-functions.py
@@ -0,0 +1,20 @@
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+def scatter_variable(name, year, ensemble):
+    fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(10, 6))
+    ax.coastlines()
+    ax.add_feature(cfeature.BORDERS, linestyle=':')
+    ax.set_global() 
+    
+    sc = ax.scatter(ds.grid1d_lon,
+                ds.grid1d_lat,
+                c=ds[name].sel(ens=ensemble, year=year),
+                cmap='rainbow',
+                s=20,
+                transform=ccrs.PlateCarree())
+    
+    plt.colorbar(sc, ax=ax, orientation='vertical', label=name)
+    
+    plt.title(f"{name} in {year}, ensemble #{ensemble}")
+    plt.show()
